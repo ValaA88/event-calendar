a0c5f14d7bf4184e1fb0241261c7a48ffd5a98d0
ValaA88t <vala.arefi@gmail.com>
2024-11-18 17:37:39 +0100

    chore: styling form from bootstrap

--- index.php
+++ index.php
@@ -55,7 +55,11 @@ if(mysqli_num_rows($result) == 0){
 
     </div>
   </nav>
-  <?= $layout ?>
+  <form enctype="multipart/form-data">
+    <a href="create.php" class="btn btn-primary" style="margin: 20px; text-align: center;">Create an Event</a>
+    <?= $layout ?>
+  </form>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
   </script>
